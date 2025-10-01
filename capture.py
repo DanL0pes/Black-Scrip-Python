0998630f9109e566b2ddb043c3bac6d68cae2726
DanL0pes <daniel.sousa@sptech.school>
2025-10-01 10:55:11 -0300

    Adicionando captura de processos ao script

--- capture.py
+++ capture.py
@@ -1,9 +1,10 @@
 import time
-import psutil
+import psutil as ps
 import pandas as pd # type: ignore
 from datetime import datetime
 import os
 
+codigoMaquina = ""
 
 def capturar(usuarioCaptura, horarioCaptura, cpuUso, memUso, diskUso):
     df = None
@@ -15,13 +16,16 @@ def capturar(usuarioCaptura, horarioCaptura, cpuUso, memUso, diskUso):
         "timestamp":[],
         "cpu":[],
         "ram":[],
-        "disco":[]
+        "disco":[],
+        "uptime": []
     }       
     dados["usuario"].append(usuarioCaptura)
     dados["timestamp"].append(horarioCaptura)
     dados["cpu"].append(cpuUso)
     dados["ram"].append(memUso)
     dados["disco"].append(diskUso)
+    uptime_seconds = (datetime.now() - datetime.fromtimestamp(ps.boot_time())).total_seconds()
+    dados["uptime"].append(uptime_seconds)
 
     if df is not None: 
         new_df = pd.DataFrame(dados)
@@ -29,8 +33,35 @@ def capturar(usuarioCaptura, horarioCaptura, cpuUso, memUso, diskUso):
     else:
         df = pd.DataFrame(dados)
 
-    return df.to_csv("relatorioMonitoramento.csv", encoding="utf-8", sep=";", mode="w", index=False) 
+    if(os.path.exists('capturaMaquina.csv')):
+        return df.to_csv("capturaMaquina.csv", mode="a", encoding="utf-8", index=False, sep=";", header=False)
+    else:
+        return df.to_csv("capturaMaquina.csv", mode="a", encoding="utf-8", index=False, sep=";")
+
+def capturarProcessos():
+    dataAtual = datetime.now();
+
+    listaProcesso = {"datetime": [], "pid": [], "codigo": [], "name": [], "status": []}
+    
+    for processo in ps.process_iter(['pid', 'name', 'status']):
+        
+        listaProcesso["datetime"].append(dataAtual)
+        listaProcesso["codigo"].append(codigoMaquina)
+        listaProcesso["pid"].append(processo.pid)
+        listaProcesso["name"].append(processo.info["name"])
+        listaProcesso["status"].append(processo.info["status"])
+        
+    dfProcesso = pd.DataFrame(listaProcesso)
+    if(os.path.exists('capturaProcesso.csv')):
+        dfProcesso.to_csv("capturaProcesso.csv", mode="a", encoding="utf-8", index=False, sep=";", header=False)
+    else:
+        dfProcesso.to_csv("capturaProcesso.csv", mode="a", encoding="utf-8", index=False, sep=";")
 
 while True:
-    capturar(psutil.users()[0].name, datetime.now().strftime("%Y-%m-%d %H:%M:%S"), psutil.cpu_percent(), psutil.virtual_memory().percent, psutil.disk_usage('/').percent)
+    capturar(codigoMaquina, 
+             datetime.now(), 
+             ps.cpu_percent(), ps.virtual_memory().percent, 
+             ps.disk_usage('/').percent
+    )
+    capturarProcessos()
     time.sleep(10)
